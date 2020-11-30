14631e8a58970617e2f0aac5f3da2035d931aed3
Saurav Raj Srivastava <sauravraj007@gmail.com>
2020-11-30 23:12:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h3>Sample Website for Experimentation</h3>
+        <img src='./hero.jpg' className="App-logo" alt="logo" />
+        <table>
+          <tr>
+            <td>
+              <input class="email-txt" type='text' id='email' />
+            </td>
+            <td>
+              <a class="signup-btn" href="cart.webex.com">Signup for free</a>
+            </td>
+          </tr>
+        </table>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
